f60e766bbb3061a3771470e55b483e7309657e0b
kateryna <k.skoryna@gmail.com>
2024-06-10 17:30:21 +0200

    fixed star component styles

--- src/components/StarComponent/StarComponent.tsx
+++ src/components/StarComponent/StarComponent.tsx
@@ -1,18 +1,13 @@
+import styles from "./StarComponent.module.css";
 function StarComponent({ rating }: { rating: number }) {
   return (
     <div>
-      <h5>Rating: {rating}</h5>
-
+      <h5 className="mb-1">Rating: {rating}</h5>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((star) => {
         return (
           <span
             key={star}
-            className="start"
-            style={{
-              cursor: "pointer",
-              color: rating >= star ? "gold" : "gray",
-              fontSize: `35px`,
-            }}
+            className={rating >= star ? styles.starRating : styles.starZero}
           >
             {" "}
             ★{" "}
