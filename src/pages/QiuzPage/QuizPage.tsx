6c4cc9e5be7280398dcdc57a94f61b3db739ef80
kateryna <k.skoryna@gmail.com>
2024-06-10 16:35:54 +0200

    fixed styles search quiz component

--- src/pages/QiuzPage/QuizPage.tsx
+++ src/pages/QiuzPage/QuizPage.tsx
@@ -4,16 +4,15 @@ import { UserQuiz } from "../../types/types";
 import { AddQuizComponent } from "../../components/AddQuizComponent/AddQuizComponent";
 import { useAuth } from "../../context/AuthContext";
 import { SearchQuizComponent } from "../../components/SearchQuizComponent/SearchQuizComponent";
-import { Container } from "react-bootstrap";
 
 export const QuizPage = () => {
   const quizes = useLoaderData() as UserQuiz[];
   const { currentUser } = useAuth();
   return (
-    <Container>
+    <>
       <SearchQuizComponent />
       {currentUser && <AddQuizComponent />}
       <QuizMainList quizes={quizes} />
-    </Container>
+    </>
   );
 };
