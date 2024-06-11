5a43aa48fff8f2a27d9a2ae03fe7a19f283879a6
KateSkoryna <KateSkoryna@users.noreply.github.com>
2024-06-11 09:29:40 +0000

    deploy: 0c7b025b78a8859f68d8a29b2790cb2e0f722677

---
+++ assets/ProtectedRoute-DWSvQmKQ.js
@@ -0,0 +1,1 @@
+import{a as s,j as o,N as r}from"./index-DyhLML9L.js";const u=({children:t})=>{const{currentUser:e}=s();return e?t:o.jsx(r,{to:"/login"})};export{u as default};
---
+++ assets/addClassnameToText-BQjz-c35.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DyhLML9L.js";import{F as a}from"./Form-D-DclIb9.js";const m=(r,s="No errors")=>o.jsx(a.Text,{className:r,children:s});export{m as a};
