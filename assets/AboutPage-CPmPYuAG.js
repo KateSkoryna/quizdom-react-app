6d78c65be7a97ebca82979df65b2c58631e2bf7b
KateSkoryna <KateSkoryna@users.noreply.github.com>
2024-06-15 16:10:17 +0000

    deploy: 657fa7b97ab5f2d311d1f01bfd551123252fc340

--- assets/AboutPage-CPmPYuAG.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BPU7orE-.js";const e=()=>t.jsx("div",{children:t.jsx("h2",{children:"About Component"})});export{e as default};
---
+++ assets/ProtectedRoute-D1J91Slw.js
@@ -0,0 +1,1 @@
+import{a as s,j as o,N as r}from"./index-DVMSeiYB.js";const u=({children:t})=>{const{currentUser:e}=s();return e?t:o.jsx(r,{to:"/login"})};export{u as default};
