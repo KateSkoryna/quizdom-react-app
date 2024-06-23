bfa07a1aa1d904accda6425701091cfbc63fa47c
KateSkoryna <KateSkoryna@users.noreply.github.com>
2024-06-23 15:47:33 +0000

    deploy: 57bea526f4729e2d8cf38faa9a6473aa1596059f

--- assets/AboutPage-tDNza3_m.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BiwfSnap.js";const e=()=>t.jsx("div",{children:t.jsx("h2",{children:"About Component"})});export{e as default};
--- assets/addClassnameToText-eR8-lbPq.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BiwfSnap.js";import{F as a}from"./Form-D53LAkfT.js";const m=(r,s="No errors")=>o.jsx(a.Text,{className:r,children:s});export{m as a};
