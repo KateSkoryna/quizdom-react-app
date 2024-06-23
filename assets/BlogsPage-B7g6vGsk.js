daa7155586be7fff081fecc19983ed8680fe994b
KateSkoryna <KateSkoryna@users.noreply.github.com>
2024-06-23 12:43:10 +0000

    deploy: 1d99fe71ee66b0c73e1cb69c14c2ff068a1d5243

---
+++ assets/BlogsPage-B7g6vGsk.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-t8ajuWjx.js";const s=()=>e.jsx(e.Fragment,{children:e.jsx("h2",{children:"Blogs Page"})});export{s as default};
--- assets/divWithClassName-BIapQoNE.js
+++ assets/divWithClassName-BIapQoNE.js
@@ -1,4 +1,4 @@
-import{v as f,r as o,j as x}from"./index-MuCFj5nI.js";var u={exports:{}};/*!
+import{v as f,r as o,j as x}from"./index-t8ajuWjx.js";var u={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
