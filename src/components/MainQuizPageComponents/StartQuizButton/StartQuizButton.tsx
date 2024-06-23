f13e45cc1163c53e8f514567c839813df36802e2
kateryna <k.skoryna@gmail.com>
2024-06-23 11:23:01 +0200

    comment component

--- src/components/MainQuizPageComponents/StartQuizButton/StartQuizButton.tsx
+++ src/components/MainQuizPageComponents/StartQuizButton/StartQuizButton.tsx
@@ -1,19 +1,19 @@
-import { Button } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
-import { useStartQuizModal } from "../../../store/store";
+// import { Button } from "react-bootstrap";
+// import Card from "react-bootstrap/Card";
+// import { useStartQuizModal } from "../../../store/store";
 
-const StartQuizButton = () => {
-  const show = useStartQuizModal((state) => state.show);
-  const handleModal = useStartQuizModal((state) => state.setShow);
+// const StartQuizButton = () => {
+//   const show = useStartQuizModal((state) => state.show);
+//   const handleModal = useStartQuizModal((state) => state.setShow);
 
-  const handleShowModal = () => {
-    handleModal(!show);
-  };
-  return (
-    <Card.Link onClick={handleShowModal} as={Button}>
-      Start Quiz
-    </Card.Link>
-  );
-};
+//   const handleShowModal = () => {
+//     handleModal(!show);
+//   };
+//   return (
+//     <Card.Link onClick={handleShowModal} as={Button}>
+//       Start Quiz
+//     </Card.Link>
+//   );
+// };
 
-export default StartQuizButton;
+// export default StartQuizButton;
