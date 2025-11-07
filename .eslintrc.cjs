76f3f66097aa2599ac0e90feabacb0587264fbea
Kateryna Skoryna <k.skoryna@gmail.com>
2025-11-07 11:56:27 +0100

    add eslint rules, update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,46 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react", "jsx-a11y"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-debugger": "warn",
+    "prefer-const": "warn",
+    "no-var": "error",
+    eqeqeq: ["warn", "always"],
+    "no-unused-expressions": "warn",
+    "jsx-a11y/no-autofocus": "warn",
   },
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// @ts-expect-error
+import eslint from "vite-plugin-eslint";
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      cache: false,
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: ["node_modules", "dist"],
+    }),
+  ],
   base: "/quizdom-react-app/",
   define: {
     "process.env": process.env,
