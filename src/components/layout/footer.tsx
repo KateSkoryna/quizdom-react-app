93ba8a2557a1435b928aafd4b49bdb0fa9f5829a
Kateryna Skoryna <k.skoryna@gmail.com>
2025-11-13 18:00:16 +0100

    Fix footer links layout and styles

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,13 +1,7 @@
 import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/esm/Nav";
-import Col from "react-bootstrap/Col";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import { FaPhoneSquareAlt } from "react-icons/fa";
 import { Container, Navbar } from "react-bootstrap";
 import styles from "../../styles/components/footer.module.scss";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -18,48 +12,29 @@ const Footer = () => {
       className={styles.footerNavbar}
     >
       <Container className={styles.footerContainer}>
-        <Nav.Link as={Link} to="/" className={styles.footerLink}>
-          About us
-        </Nav.Link>
         <Nav className={styles.footerContactNavbar}>
-          <Col xs={3} sm={3} md={3}>
-            <Nav.Link
-              href="https://www.linkedin.com/in/kateskoryna/"
-              target="_blank"
-              className={styles.footerContactLink}
-            >
-              <FaLinkedin className={styles.footerIcon} />
-            </Nav.Link>
-          </Col>
-          <Col xs={3} sm={3} md={3}>
-            <Nav.Link
-              href="https://github.com/kateskoryna"
-              target="_blank"
-              className={styles.footerContactLink}
-            >
-              <FaGithub className={styles.footerIcon} />
-            </Nav.Link>
-          </Col>
-          <Col xs={3} sm={3} md={3}>
-            <Nav.Link
-              href="tel:+4916099814255"
-              target="_blank"
-              className={styles.footerContactLink}
-            >
-              <FaPhoneSquareAlt className={styles.footerIcon} />
-            </Nav.Link>
-          </Col>
-          <Col xs={3} sm={3} md={3}>
-            <Nav.Link
-              href="mailto:k.skoryna@gmail.com"
-              target="_blank"
-              className={styles.footerContactLink}
-            >
-              <MdEmail className={styles.footerIcon} />
-            </Nav.Link>
-          </Col>
+          <Nav.Link
+            href="https://www.linkedin.com/in/kateskoryna/"
+            target="_blank"
+            className={`${styles.footerContactLink} ${styles.linkedinIcon}`}
+          />
+          <Nav.Link
+            href="https://github.com/kateskoryna"
+            target="_blank"
+            className={`${styles.footerContactLink} ${styles.githubIcon}`}
+          />
+          <Nav.Link
+            href="tel:+4916099814255"
+            target="_blank"
+            className={`${styles.footerContactLink} ${styles.phoneIcon}`}
+          />
+          <Nav.Link
+            href="mailto:k.skoryna@gmail.com"
+            target="_blank"
+            className={`${styles.footerContactLink} ${styles.emailIcon}`}
+          />
         </Nav>
-        <Card.Text>Copyright © 2025 by Kate Skoryna.</Card.Text>
+        <Card.Text>Copyright © 2025 by Kate Skoryna</Card.Text>
       </Container>
     </Navbar>
   );
