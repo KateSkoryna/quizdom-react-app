4fe6a8785264739f1e6504a72ec766b272d39178
Kateryna Skoryna <k.skoryna@gmail.com>
2025-11-18 17:35:29 +0100

    Create blockquote component, simplify perent component

---
+++ src/components/common/blockQuote.tsx
@@ -0,0 +1,19 @@
+import { JOKE } from "../../helpers/jokes";
+import styles from "../../styles/pages/home.module.scss";
+
+const BlockQuote = () => {
+  return (
+    <blockquote className="mb-0 fs-4 d-block">
+      <p
+        className={
+          JOKE.joke.length > 100 ? styles.heroJokeTextBig : styles.heroJokeText
+        }
+      >
+        {JOKE.joke}
+      </p>
+      <footer className={styles.heroJokeAuthor}>{`— ${JOKE.author}`}</footer>
+    </blockquote>
+  );
+};
+
+export default BlockQuote;
--- src/components/layout/heroContainer.tsx
+++ src/components/layout/heroContainer.tsx
@@ -1,35 +1,21 @@
 import Card from "react-bootstrap/Card";
-import { JOKE } from "../../helpers/jokes";
 import styles from "../../styles/pages/home.module.scss";
 import hero from "../../assets/hero-big.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Container } from "react-bootstrap";
+import BlockQuote from "../common/blockQuote";
 
 const HeroContainer = () => {
   return (
     <Card className={styles.heroCard}>
       <LazyLoadImage src={hero} className={styles.heroImg} alt="hero" />
-
       <Card.ImgOverlay className={styles.overlay}>
         <Container>
           <Card.Body className={styles.heroCardBody}>
             <Card.Title className={styles.heroCardTitle}>
               Dive into the Depths <br /> of Coding Wisdom
+              <BlockQuote />
             </Card.Title>
-            <blockquote className="mb-0 fs-4 d-block">
-              <p
-                className={
-                  JOKE.joke.length > 100
-                    ? styles.heroJokeTextBig
-                    : styles.heroJokeText
-                }
-              >
-                {JOKE.joke}
-              </p>
-              <footer
-                className={styles.heroJokeAuthor}
-              >{`— ${JOKE.author}`}</footer>
-            </blockquote>
           </Card.Body>
         </Container>
       </Card.ImgOverlay>
