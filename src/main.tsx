91380a52b61bb4bbf7b6f7274ca6e1612d372ad5
Kateryna Skoryna <k.skoryna@gmail.com>
2025-11-18 19:53:13 +0100

    Change router type

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { lazy } from "react";
 import ReactDOM from "react-dom/client";
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
   Navigate,
 } from "react-router-dom";
@@ -17,61 +17,58 @@ const NewsPage = lazy(() => import("./pages/news"));
 const NotFoundPage = lazy(() => import("./pages/notFound"));
 const ProtectedRoute = lazy(() => import("./components/common/protectedRoute"));
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="quizes" replace />,
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="quizes" replace />,
+      },
+      {
+        path: "quizes",
+        element: <HomePage />,
+      },
+      {
+        path: "news",
+        element: <NewsPage />,
+        loader: async ({ request }) => {
+          const url = new URL(request.url);
+          let searchQuery = url.searchParams.get("query") || "";
+          let searchCategory = url.searchParams.get("category");
+          if (!searchQuery) {
+            searchQuery = "none";
+          }
+          if (!searchCategory) {
+            searchCategory = "technology";
+          }
+          return await getMediaNews(searchCategory, searchQuery);
         },
-        {
-          path: "quizes",
-          element: <HomePage />,
-        },
-        {
-          path: "news",
-          element: <NewsPage />,
-          loader: async ({ request }) => {
-            const url = new URL(request.url);
-            let searchQuery = url.searchParams.get("query") || "";
-            let searchCategory = url.searchParams.get("category");
-            if (!searchQuery) {
-              searchQuery = "none";
-            }
-            if (!searchCategory) {
-              searchCategory = "technology";
-            }
-            return await getMediaNews(searchCategory, searchQuery);
-          },
-        },
-        {
-          path: "signup",
-          element: <SignupPage />,
-        },
-        {
-          path: "login",
-          element: <LoginPage />,
-        },
-        {
-          path: "user",
-          element: (
-            <ProtectedRoute>
-              <UserPage />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "*",
-          element: <NotFoundPage />,
-        },
-      ],
-    },
-  ],
-  { basename: "/quizdom-react-app" }
-);
+      },
+      {
+        path: "signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "user",
+        element: (
+          <ProtectedRoute>
+            <UserPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
