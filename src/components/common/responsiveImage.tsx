89e1b6d244287d7af180d7fc9b61cc46b9c65003
Kateryna Skoryna <k.skoryna@gmail.com>
2025-11-19 13:46:28 +0100

    Fix hero img

--- src/components/common/responsiveImage.tsx
+++ src/components/common/responsiveImage.tsx
@@ -1,9 +1,4 @@
-// @ts-expect-error - vite-imagetools query parameters
-import heroSmall from "../../assets/hero.webp?w=300";
-// @ts-expect-error - vite-imagetools query parameters
-import heroMedium from "../../assets/hero.webp?w=600";
-// @ts-expect-error - vite-imagetools query parameters
-import heroLarge from "../../assets/hero.webp?w=900";
+import hero from "../../assets/hero.webp";
 
 interface ResponsiveImageProps {
   className?: string;
@@ -13,8 +8,8 @@ interface ResponsiveImageProps {
 const ResponsiveImage = ({ className, alt = "hero" }: ResponsiveImageProps) => {
   return (
     <img
-      src={heroSmall}
-      srcSet={`${heroSmall} 300w, ${heroMedium} 600w, ${heroLarge} 900w`}
+      src={hero}
+      srcSet={`${hero} 300w, ${hero} 600w, ${hero} 900w`}
       sizes="(max-width: 600px) 300px, (max-width: 900px) 600px, 900px"
       className={className}
       alt={alt}
