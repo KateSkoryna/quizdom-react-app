5e5386a8e8b536f4f42384b1f5163a6400cd2a67
Kateryna Skoryna <k.skoryna@gmail.com>
2025-12-02 17:32:08 +0100

    add user check to add quiz component

--- src/components/quiz/addQuizComponent.tsx
+++ src/components/quiz/addQuizComponent.tsx
@@ -2,8 +2,11 @@ import { useOpenQuizModal } from "../../store/store";
 import QuizModal from "../modal/quizModal";
 import { Button, Container } from "react-bootstrap";
 import UserQuizList from "../user/userQuizList";
+import styles from "../../styles/components/addQuiz.module.scss";
+import { useAuthStore } from "../../store/AuthStore";
 
 const AddQuizComponent = () => {
+  const currentUser = useAuthStore((state) => state.currentUser);
   const show = useOpenQuizModal((state) => state.show);
   const handleModal = useOpenQuizModal((state) => state.setShow);
 
@@ -13,14 +16,20 @@ const AddQuizComponent = () => {
   const handleShowModal = () => {
     handleModal(true);
   };
-  return (
-    <Container className="text-end">
-      <Button onClick={handleShowModal}>Add Quiz</Button>
+  return currentUser ? (
+    <Container className={styles.addQuizContainer}>
+      <Button onClick={handleShowModal} className={styles.ctaButton}>
+        <span className={styles.plusIcon}>+</span>
+        Create Your Quiz
+      </Button>
+      <p className={styles.ctaSubtext}>
+        Share your knowledge and challenge others!
+      </p>
 
       <UserQuizList />
       <QuizModal showModal={show} handleCloseModal={handleCloseModal} />
     </Container>
-  );
+  ) : null;
 };
 
 export default AddQuizComponent;
