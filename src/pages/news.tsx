53bfdb7a08db95346f8c55a9d89bbbca453a267c
Kateryna Skoryna <k.skoryna@gmail.com>
2025-12-05 14:59:32 +0100

    delete uuid from project

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -2,13 +2,15 @@ import NewsListComponent from "../components/news/newsList";
 import { useLoaderData, useNavigation } from "react-router-dom";
 import { Article, NEWS_CATEGORY } from "../types/types";
 import SearchComponent from "../components/news/searchComponent";
-import uuid from "react-uuid";
 import Loader from "../components/common/loader";
 
 const NewsPage = () => {
   const navigation = useNavigation();
   const newsList: Article[] = useLoaderData() as Article[];
-  const news = newsList.map((article) => ({ ...article, id: uuid() }));
+  const news = newsList.map((article, index) => ({
+    ...article,
+    id: `${new Date().getTime()}-${index}`
+  }));
   const values: string[] = Object.values(NEWS_CATEGORY);
 
   return (
