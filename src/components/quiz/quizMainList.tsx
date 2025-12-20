b058472e200709eefa14d0b89ddb4f88a1342e98
Kateryna Skoryna <k.skoryna@gmail.com>
2025-12-20 16:42:59 +0100

    move logic to child component

--- src/components/quiz/quizMainList.tsx
+++ src/components/quiz/quizMainList.tsx
@@ -1,18 +1,36 @@
-import { useQuizesStore } from "../../store/quizeStore";
-import { useAuthStore } from "../../store/AuthStore";
+import { useQuizStore } from "../../store/quizStore";
+import { useAuthStore } from "../../store/authStore";
 import QuizMainListItem from "./quizItem/quizMainListItem";
 import AddQuizCard from "./quizItem/addQuizCard";
 import { Container, Card } from "react-bootstrap";
 import styles from "../../styles/components/quizCard.module.scss";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import Loader from "../common/loader";
 
 const QuizMainList = () => {
-  const quizes = useQuizesStore((state) => state.quizes);
+  const quizzes = useQuizStore((state) => state.quizzes);
   const currentUser = useAuthStore((state) => state.currentUser);
+  const isLoading = useQuizStore((state) => state.isLoading);
+  const [searchParams] = useSearchParams();
+  const getQuizzes = useQuizStore((state) => state.getQuizzes);
+
+  const searchCategory = searchParams.get("category") || null;
+  const searchComplexity = searchParams.get("complexity") || null;
+
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      await getQuizzes(searchCategory, searchComplexity);
+    };
+    fetchQuizzes();
+  }, [searchCategory, searchComplexity, getQuizzes]);
+
+  if (isLoading) return <Loader />;
 
   return (
     <Container className={styles.gridContainer}>
       {currentUser && <AddQuizCard />}
-      {quizes.map((quiz) => (
+      {quizzes.map((quiz) => (
         <Card key={quiz.id} className={styles.gridCard}>
           <QuizMainListItem quiz={quiz} />
         </Card>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,34 +1,8 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
 import HeroContainer from "../components/layout/heroContainer";
-import { useQuizesStore } from "../store/quizeStore";
 import SearchQuizComponent from "../components/quiz/searchQuizComponent";
 import QuizMainList from "../components/quiz/quizMainList";
 
 const HomePage = () => {
-  const [searchParams] = useSearchParams();
-  const getQuizes = useQuizesStore((state) => state.getQuizes);
-  const getQuizesByCategoryAndComplexity = useQuizesStore(
-    (state) => state.getQuizesByCategoryAndComplexity
-  );
-
-  const searchCategory = searchParams.get("category") || null;
-  const searchComplexity = searchParams.get("complexity") || null;
-
-  useEffect(() => {
-    if (!searchCategory && !searchComplexity) {
-      const fetchQuizes = async () => {
-        await getQuizes();
-      };
-      fetchQuizes();
-    } else {
-      const fetchQuizesByCategoryAndComplexity = async () => {
-        await getQuizesByCategoryAndComplexity(searchCategory, searchComplexity);
-      };
-      fetchQuizesByCategoryAndComplexity();
-    }
-  }, [searchCategory, searchComplexity, getQuizes, getQuizesByCategoryAndComplexity]);
-
   return (
     <>
       <HeroContainer />
