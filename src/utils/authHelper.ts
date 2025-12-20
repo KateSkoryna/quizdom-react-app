f8753daf8dc2c036705c8d270de10c74f716b581
Kateryna Skoryna <k.skoryna@gmail.com>
2025-12-20 16:51:06 +0100

    fix eslint warning

--- src/utils/authHelper.ts
+++ src/utils/authHelper.ts
@@ -60,6 +60,7 @@ export const ensureUserDocument = async (
     // Create or update user document
     await setDoc(userRef, userData, { merge: true });
   } catch (error) {
+    console.log(error);
     throw new Error("Failed to create user profile");
   }
 };
