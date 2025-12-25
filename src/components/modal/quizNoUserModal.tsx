b1a5150a24d3d5fa33b4bf3f6ea1bf638274aa5e
Kateryna Skoryna <k.skoryna@gmail.com>
2025-12-25 13:10:17 +0100

    add share component to modal

--- src/components/modal/quizNoUserModal.tsx
+++ src/components/modal/quizNoUserModal.tsx
@@ -1,31 +1,19 @@
 import { useState } from "react";
 import FavoriteElement from "../quiz/quizItem/favoriteElement";
-import { MdShare } from "react-icons/md";
+
 import NavigateUserModal from "./navigateUserModal";
 import styles from "../../styles/components/quizCard.module.scss";
+import ShareElement from "../quiz/quizItem/shareElement";
 
 const QuizNoUserModal = ({ id }: { id: string }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleModalToggle = () => setIsModalOpen(!isModalOpen);
 
-  const handleShare = () => {
-    if (navigator.share) {
-      navigator.share({
-        title: "Check out this quiz!",
-        url: window.location.href,
-      });
-    } else {
-      navigator.clipboard.writeText(window.location.href);
-    }
-  };
-
   return (
     <>
       <div className={styles.actionButtons}>
         <FavoriteElement quizId={id} onAuthRequired={handleModalToggle} />
-        <button onClick={handleShare} className={styles.shareButton} aria-label="Share quiz">
-          <MdShare className={styles.shareIcon} />
-        </button>
+        <ShareElement quizId={id} onAuthRequired={handleModalToggle} />
       </div>
       {isModalOpen && <NavigateUserModal handleClose={handleModalToggle} show={isModalOpen} />}
     </>
